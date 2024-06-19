d3be4a18d7bb8ed1396ed7a168a0f918ef27a576
ameknite <code.affidavit841@passinbox.com>
2024-06-18 21:35:21 -0600

    templates -> template

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 // SPDX-FileCopyrightText: Copyright (c) AmeKnite
 
-//! Documentation for the crate: A rust ci/cd templates
+//! Documentation for the crate: A rust ci/cd template
 fn main() {
     println!("Hello, world!");
 }
